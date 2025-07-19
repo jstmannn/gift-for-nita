1ab71d8f9b8d51232d5b14de5cf4cfdb6503669f
Nur Rohman <stillaway0707@gmail.com>
2025-07-19 19:24:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const message = `Halo Nita, aku seneng bisa kenal kamu dan mabar sama kamu siang ini.\n\nAku mau kasih tau kamu duniaku...\n\nDunia aku terlalu sempit dan jarang orang bisa kenal aku lebih jauh.\n\n Aku harap datangnya kamu bukan cuma sementara karena aku juga butuh someone to talk. \n\nAku juga seneng kalo bisa dapet temen mabar dan selalu contact-an.\n\nJadi, kamu bisa hubungin aku kapanpun kamu butuh, aku mau jadi apapun yg kamu butuhin nantinya.`;
 
 function showLetter() {
+   const music = document.getElementById("bgMusic");
+        music.play();
   document.getElementById("introText").style.opacity = 0;
   document.querySelector(".btn").style.display = "none";
 
@@ -25,12 +27,5 @@ function showLetter() {
   }, 100);
 }
 
-function showWhatsAppButton() {
-  const waButton = document.createElement("a");
-  waButton.href = "https://wa.me/6285718993124";
-  waButton.target = "_blank";
-  waButton.className = "wa-button";
-  waButton.innerText = "Hubungi Aku di WhatsApp";
-
-  document.getElementById("letterBox").appendChild(waButton);
+ showInstagramButton();
 }
